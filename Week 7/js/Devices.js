3625c91de31845691340621cce2a450af7565bb9
FAlexandrou <FAlexandrou@uclan.ac.uk>
2018-11-07 16:29:08 +0200

    Week 7.

---
+++ Week 7/js/Devices.js
@@ -0,0 +1,96 @@
+//Brands:
+const SAMSUNG = "Samsung";
+const APPLE = "Apple";
+const LG = "LG";
+const SONY = "Sony";
+const HUAWEI = "Huawei";
+const ALCATEL = "Alcatel";
+
+class Device {
+    constructor(brand, name) {
+        this._brand = brand;
+        this._name = name;
+    }
+    get brand() {
+        return this._brand;
+    }
+    get name() {
+        return this._name;
+    }
+    set brand(value) {
+        this._brand = brand;
+    }
+    set name(value) {
+        this._name = name;
+    }
+}
+
+var ALL_BRANDS = [
+    SAMSUNG,
+    APPLE,
+    LG,
+    SONY,
+    HUAWEI,
+    ALCATEL
+];
+
+//Devices:
+var ALL_DEVICES = [
+    new Device(SAMSUNG, "Galaxy S8"),
+    new Device(SAMSUNG, "Galaxy S8 Plus"),
+    new Device(SAMSUNG, "Galaxy S9"),
+    new Device(SAMSUNG, "Galaxy S9 Plus"),
+    new Device(SAMSUNG, "Galaxy Note 8"),
+    new Device(SAMSUNG, "Galaxy Note 9"),
+    new Device(SAMSUNG, "Galaxy Tab S4"),
+    new Device(APPLE, "iPhone X"),
+    new Device(APPLE, "iPhone 8 Plus"),
+    new Device(APPLE, "iPhone 8"),
+    new Device(APPLE, "iPhone 7"),
+    new Device(APPLE, "iPhone 7 Plus"),
+    new Device(APPLE, "iPhone 6"),
+    new Device(APPLE, "iPad 2017"),
+    new Device(APPLE, "iPad 2018"),
+    new Device(APPLE, "iPad Mini 4"),
+    new Device(LG, "G7 ThinQ"),
+    new Device(LG, "Q7"),
+    new Device(LG, "Q6"),
+    new Device(LG, "V30"),
+    new Device(LG, "K9"),
+    new Device(SONY, "Xperia XZ2"),
+    new Device(SONY, "Xperia XA2"),
+    new Device(SONY, "Xperia XA1"),
+    new Device(SONY, "Xperia XA1 Ultra"),
+    new Device(HUAWEI, "P20 Pro"),
+    new Device(HUAWEI, "P20 Lite"),
+    new Device(HUAWEI, "Y7 Prime 2018"),
+    new Device(HUAWEI, "Y6 2018"),
+    new Device(HUAWEI, "Mate 10 Lite"),
+    new Device(ALCATEL, "U5 HD Premium"),
+    new Device(ALCATEL, "U5"),
+    new Device(ALCATEL, "A5"),
+];
+
+//My code
+var listElements = document.getElementsByClassName("list");
+
+for (var element of listElements){
+    element.style.border="2px solid blue";
+    element.style.fontSize = "20px";
+}
+
+var brandOptionList = document.getElementById("brandOption");
+var deviceOptionList = document.getElementById("deviceOption");
+for(let i = 0 ; i<ALL_BRANDS.length; i++){
+    var newBrandOption =  document.createElement("option");
+    newBrandOption.text = ALL_BRANDS[i];
+    newBrandOption.value = ALL_BRANDS[i].value;
+    brandOptionList.appendChild(newBrandOption);
+}
+
+for(let i = 0; i<ALL_DEVICES.length; i++){
+    var newDeviceOption = document.createElement("option");
+    newDeviceOption.text = ALL_DEVICES[i].name;
+    newDeviceOption.value = ALL_DEVICES[i].name;
+    deviceOptionList.appendChild(newDeviceOption);
+}
